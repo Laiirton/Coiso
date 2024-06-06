406032e524a2e743f6435620f032206341045343
Laiirton <130435095+Laiirton@users.noreply.github.com>
2024-06-06 02:21:34 -0300

    initial

---
+++ src/index.ts
@@ -0,0 +1,43 @@
+import { create } from '@wppconnect-team/wppconnect';
+
+function start(client: any) {
+  // Your start function implementation goes here
+}
+
+create({
+  session: 'sessionName', //Pass the name of the client you want to start the bot
+  catchQR: (base64Qrimg: any, asciiQR: any, attempts: any, urlCode: any) => {
+    console.log('Number of attempts to read the qrcode: ', attempts);
+    console.log('Terminal qrcode: ', asciiQR);
+    console.log('base64 image string qrcode: ', base64Qrimg);
+    console.log('urlCode (data-ref): ', urlCode);
+  },
+  statusFind: (statusSession: any, session: any) => {
+    console.log('Status Session: ', statusSession); //return isLogged || notLogged || browserClose || qrReadSuccess || qrReadFail || autocloseCalled || desconnectedMobile || deleteToken
+    //Create session wss return "serverClose" case server for close
+    console.log('Session name: ', session);
+  },
+  headless: true, // Headless chrome
+  devtools: false, // Open devtools by default
+  useChrome: true, // If false will use Chromium instance
+  debug: false, // Opens a debug session
+  logQR: true, // Logs QR automatically in terminal
+  browserWS: '', // If u want to use browserWSEndpoint
+  browserArgs: [''], // Parameters to be added into the chrome browser instance
+  puppeteerOptions: {}, // Will be passed to puppeteer.launch
+  disableWelcome: false, // Option to disable the welcoming message which appears in the beginning
+  updatesLog: true, // Logs info updates automatically in terminal
+  autoClose: 60000, // Automatically closes the wppconnect only when scanning the QR code (default 60 seconds, if you want to turn it off, assign 0 or false)
+  tokenStore: 'file', // Define how work with tokens, that can be a custom interface
+  folderNameToken: './src/tokens/', //folder name when saving tokens
+  // BrowserSessionToken
+  // To receive the client's token use the function await clinet.getSessionTokenBrowser()
+  sessionToken: {
+    WABrowserId: '"UnXjH....."',
+    WASecretBundle: '{"key":"+i/nRgWJ....","encKey":"kGdMR5t....","macKey":"+i/nRgW...."}',
+    WAToken1: '"0i8...."',
+    WAToken2: '"1@lPpzwC...."',
+  }
+})
+.then((client) => start(client))
+.catch((error: any) => console.log(error));
